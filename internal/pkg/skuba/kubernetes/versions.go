evrardjp/skuba

commit 598fd04042d4a5f494bc0809c3b695e883d78fe3

Update to Kubernetes v1.18.2

internal/pkg/skuba/kubernetes/versions.go
@@ -85,6 +85,30 @@ type ClusterAddonsKnownVersions = func(clusterVersion *version.Version) AddonsVe
 
 var (
 	supportedVersions = KubernetesVersions{
+		"1.18.2": KubernetesVersion{
+			ComponentHostVersion: ComponentHostVersion{
+				KubeletVersion:          "1.18.2",
+				ContainerRuntimeVersion: "1.18.0",
+			},
+			ComponentContainerVersion: ComponentContainerVersion{
+				APIServer:         &ContainerImageTag{Name: "api-server", Tag: "v1.18.2"},
+				ControllerManager: &ContainerImageTag{Name: "controller-manager", Tag: "v1.18.2"},
+				Scheduler:         &ContainerImageTag{Name: "scheduler", Tag: "v1.18.2"},
+				Proxy:             &ContainerImageTag{Name: "proxy", Tag: "v1.18.2"},
+				Etcd:              &ContainerImageTag{Name: "etcd", Tag: "3.4.3"},
+				CoreDNS:           &ContainerImageTag{Name: "coredns", Tag: "1.6.7"},
+				Pause:             &ContainerImageTag{Name: "pause", Tag: "3.2"},
+				Tooling:           &ContainerImageTag{Name: "skuba-tooling", Tag: "0.1.0"},
+			},
+			AddonsVersion: AddonsVersion{
+				Cilium:        &AddonVersion{"1.5.3", 2},
+				Kured:         &AddonVersion{"1.3.0", 4},
+				Dex:           &AddonVersion{"2.23.0", 6},
+				Gangway:       &AddonVersion{"3.1.0-rev4", 4},
+				MetricsServer: &AddonVersion{"0.3.6", 0},
+				PSP:           &AddonVersion{"", 2},
+			},
+		},
 		"1.17.4": KubernetesVersion{
 			ComponentHostVersion: ComponentHostVersion{
 				KubeletVersion:          "1.17.4",

internal/pkg/skuba/kubernetes/versions_test.go
@@ -374,7 +374,7 @@ func TestMajorMinorVersion(t *testing.T) {
 func TestEnsureHyperKubeIsRemovedForSupportedVersions(t *testing.T) {
 	for kubernetesVersionStr := range supportedVersions {
 		kubernetesVersion := version.MustParseSemantic(kubernetesVersionStr)
-		versionComparedTo118, err := kubernetesVersion.Compare("1.18.0")
+		versionComparedTo118, err := kubernetesVersion.Compare("1.18.2")
 		if err != nil {
 			t.Fatalf("our versions should always be valid semver and something bad happened: %+v", err)
 		}

commit a08c65fecf75cff193e8e431f3349cd19e26b6a3

versions: Fix Cilium version for k8s v1.18.2
Fixes: 598fd04042d4 ("Update to Kubernetes v1.18.2")

Signed-off-by: Michal Rostecki <mrostecki@suse.de>

internal/pkg/skuba/kubernetes/versions.go
@@ -101,7 +101,7 @@ var (
 				Tooling:           &ContainerImageTag{Name: "skuba-tooling", Tag: "0.1.0"},
 			},
 			AddonsVersion: AddonsVersion{
-				Cilium:        &AddonVersion{"1.5.3", 3},
+				Cilium:        &AddonVersion{"1.6.6", 3},
 				Kured:         &AddonVersion{"1.3.0", 5},
 				Dex:           &AddonVersion{"2.23.0", 6},
 				Gangway:       &AddonVersion{"3.1.0-rev4", 4},